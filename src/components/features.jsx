1c661fb13eb3e278e0ad67f0b65ff68355ea385e
Wadhah <wadhahagoubi@gmail.com>
2024-10-16 16:55:38 +0100

    landing page

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -7,7 +7,7 @@ export const Features = (props) => {
         <div className="col-md-10 col-md-offset-1 section-title">
           <h2>Features</h2>
         </div>
-        <div className="row">
+        <div className="row" >
           {props.data
             ? props.data.map((d, i) => (
                 <div key={`${d.title}-${i}`} className="col-xs-6 col-md-3">
